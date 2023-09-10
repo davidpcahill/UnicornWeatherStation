2036485b2ff3f5b507bcb0d05a887d23307c5b8d
Tag Wolf <david.cahill@gmail.com>
2023-09-10 04:18:21 -0700

    Update weatherstation.py

--- weatherstation.py
+++ weatherstation.py
@@ -11,9 +11,11 @@ WIFI_PASSWORD = "YOUR WIFI PASSWORD"  # Replace with your password
 OPEN_WEATHER_MAP_API_KEY = "YOUR OPENWEATHER API KEY"  # Replace with your API key
 CITY = "San Jose"  # Replace with your city
 COUNTRY_CODE = "US"  # Replace with your country code
-URL = f'http://api.openweathermap.org/data/2.5/weather?q={CITY},{COUNTRY_CODE}&units=imperial&APPID={OPEN_WEATHER_MAP_API_KEY}'
+URL = f"http://api.openweathermap.org/data/2.5/weather?q={CITY},{COUNTRY_CODE}&units=imperial&APPID={OPEN_WEATHER_MAP_API_KEY}"
 UTC_OFFSET = -7 * 3600  # Example for PDT. Adjust as needed.
-BUTTON_PRESS_TIMEOUT = 10  # Time in seconds after which the clock will be displayed again
+BUTTON_PRESS_TIMEOUT = (
+    10  # Time in seconds after which the clock will be displayed again
+)
 WEATHER_FETCH_INTERVAL = 60
 
 gu = GalacticUnicorn()
@@ -30,13 +32,14 @@ display_clock = True
 last_button_press_time = None
 last_weather_fetch_time = 0
 
+
 def connect_to_wifi():
     wlan = network.WLAN(network.STA_IF)
     if wlan.isconnected():
         return True
 
     wlan.active(True)
-    wlan.config(pm=0xa11140)
+    wlan.config(pm=0xA11140)
     wlan.connect(WIFI_SSID, WIFI_PASSWORD)
 
     max_wait = 100
@@ -48,6 +51,7 @@ def connect_to_wifi():
 
     return wlan.isconnected()
 
+
 def sync_time():
     if connect_to_wifi():
         try:
@@ -57,6 +61,7 @@ def sync_time():
             print("Time set")
         except OSError:
             pass
+
 
 def get_weather_data():
     global last_weather_fetch_time, weather_data
@@ -77,51 +82,73 @@ def get_weather_data():
     else:
         return
 
+
 def hpa_to_inHg(hpa):
     return hpa * 0.02953
+
 
 def get_temperature_color(temp):
     if temp <= 32:
         return graphics.create_pen(255, 255, 255)  # White
     elif 32 < temp <= 65:
         decrease_value = int((temp - 32) * (255 / 33))
-        return graphics.create_pen(255 - decrease_value, 255 - decrease_value, 255)  # Transition from white to blue
+        return graphics.create_pen(
+            255 - decrease_value, 255 - decrease_value, 255
+        )  # Transition from white to blue
     elif 65 < temp <= 72:
         return graphics.create_pen(0, 0, 255)  # Blue
     elif 72 < temp <= 85:
         red_value = int((temp - 72) * (255 / 13))
-        return graphics.create_pen(red_value, red_value, 255 - red_value)  # Transition from blue to yellow
+        return graphics.create_pen(
+            red_value, red_value, 255 - red_value
+        )  # Transition from blue to yellow
     else:
         return graphics.create_pen(255, 0, 0)  # Red
+
 
 def get_humidity_color(humidity):
     green_value = int(255 - humidity * 2.55)
     return graphics.create_pen(0, green_value, 255)  # Cyan to blue gradient
 
+
 def get_wind_speed_color(speed):
     green_value = int(255 - speed * 2.55)  # Assuming max speed is 100mph for full blue
     return graphics.create_pen(0, green_value, 255)  # Cyan to blue gradient
+
 
 def get_pressure_color(pressure):
     if pressure <= 980:
         return graphics.create_pen(0, 0, 255)  # Dark blue for low pressure
     elif 980 < pressure <= 1013:
         blue_value = int(255 - (pressure - 980) * (255 / 33))
-        return graphics.create_pen(0, 255 - blue_value, blue_value)  # Gradient from dark blue to light blue
+        return graphics.create_pen(
+            0, 255 - blue_value, blue_value
+        )  # Gradient from dark blue to light blue
     elif 1013 < pressure <= 1040:
         green_value = int((pressure - 1013) * (255 / 27))
-        return graphics.create_pen(0, green_value, 255 - green_value)  # Gradient from light blue to green
+        return graphics.create_pen(
+            0, green_value, 255 - green_value
+        )  # Gradient from light blue to green
     else:
         return graphics.create_pen(0, 255, 0)  # Bright green for high pressure
+
 
 def get_cloud_coverage_color(coverage):
     MIN_GRAY = 50
     gray_value = int(MIN_GRAY + (255 - MIN_GRAY) * (1 - coverage / 100))
-    return graphics.create_pen(gray_value, gray_value, gray_value)  # Adjusted gray gradient for cloud coverage
+    return graphics.create_pen(
+        gray_value, gray_value, gray_value
+    )  # Adjusted gray gradient for cloud coverage
+
 
 def get_visibility_color(visibility):
-    green_value = int(visibility * 0.0255)  # Assuming max visibility of 10,000 meters for full green
-    return graphics.create_pen(0, green_value, 255 - green_value)  # Blue to green gradient for visibility
+    green_value = int(
+        visibility * 0.0255
+    )  # Assuming max visibility of 10,000 meters for full green
+    return graphics.create_pen(
+        0, green_value, 255 - green_value
+    )  # Blue to green gradient for visibility
+
 
 def get_clock_color(hour):
     if 0 <= hour < 6:
@@ -139,6 +166,7 @@ def get_clock_color(hour):
         blue_value = int(255 - (hour - 18) * (127 / 6))
         return graphics.create_pen(255, 0, blue_value)
 
+
 def get_date_color(month):
     if month in [12, 1, 2]:  # Winter
         return graphics.create_pen(0, 0, 255)
@@ -152,6 +180,7 @@ def get_date_color(month):
         green_value = int(128 + (month - 9) * (42 / 3))
         return graphics.create_pen(red_value, green_value, 0)
 
+
 def display_time():
     _, _, _, hour, minute, second, _, _ = time.localtime()
     clock = "{:02}:{:02}:{:02}".format(hour, minute, second)
@@ -164,11 +193,12 @@ def display_time():
     clock_color = get_clock_color(hour)
     outline_text(clock, x, y, clock_color, BLACK)
 
+
 def display_date():
     # Fetch current date
     year, month, day, _, _, _, _, _ = time.localtime()
     date_str = "{:04}-{:02}-{:02}".format(year, month, day)
-    
+
     # Calculate text position so that it is centered
     w = graphics.measure_text(date_str, 1)
     x = int(width / 2 - w / 2 + 1)
@@ -177,23 +207,24 @@ def display_date():
     date_color = get_date_color(month)
     outline_text(date_str, x, y, date_color, BLACK)
 
+
 def display_weather(button):
     # Fetch weather data
     weather_data = get_weather_data()
-    
+
     if not weather_data:
         outline_text("No Data", 11, 2)
         return
 
     if button == "a":
         # Display Weather Icon, Temperature, and Humidity
-        icon_id = weather_data['weather'][0]['icon']
+        icon_id = weather_data["weather"][0]["icon"]
         icon_file = f"{icon_id}.jpg"
         # Assuming you have a function to display the jpeg image
         display_jpeg(icon_file, 0, 0)
 
-        temp = round(weather_data['main']['temp'])
-        humidity = weather_data['main']['humidity']
+        temp = round(weather_data["main"]["temp"])
+        humidity = weather_data["main"]["humidity"]
         temp_color = get_temperature_color(temp)
         outline_text(f"{temp}°F", 14, 2, temp_color, BLACK)
         humidity_color = get_humidity_color(humidity)
@@ -201,66 +232,78 @@ def display_weather(button):
 
     elif button == "b":
         # Display Min Temp and Max Temp
-        temp_min = round(weather_data['main']['temp_min'])
-        temp_max = round(weather_data['main']['temp_max'])
+        temp_min = round(weather_data["main"]["temp_min"])
+        temp_max = round(weather_data["main"]["temp_max"])
         # Display thermometer icons (assuming you have them as jpeg)
-        #display_jpeg("cold_thermometer.jpg", 10, 10)
-        #display_jpeg("hot_thermometer.jpg", 10, 50)
+        # display_jpeg("cold_thermometer.jpg", 10, 10)
+        # display_jpeg("hot_thermometer.jpg", 10, 50)
         outline_text(f"{temp_min}°F", 7, 2, BLUE, BLACK)
         outline_text(f"{temp_max}°F", 30, 2, RED, BLACK)
-        
+
     elif button == "c":
         # Display Pressure and Wind Speed and Direction
-        pressure = weather_data['main']['pressure']
+        pressure = weather_data["main"]["pressure"]
         pressure_inHg = hpa_to_inHg(pressure)
         pressure_color = get_pressure_color(pressure)
-        wind_speed = round(weather_data['wind']['speed'])
-        wind_dir = weather_data['wind']['deg']
+        wind_speed = round(weather_data["wind"]["speed"])
+        wind_dir = weather_data["wind"]["deg"]
         wind_speed_color = get_wind_speed_color(wind_speed)
-        #outline_text(f"{pressure}h", 2, 2, pressure_color, BLACK)
+        # outline_text(f"{pressure}h", 2, 2, pressure_color, BLACK)
         outline_text(f"{pressure_inHg:.2f}", 2, 2, get_pressure_color(pressure))
-        outline_text(f"{wind_speed}mph", 25, 2, wind_speed_color, BLACK)
+        outline_text(f"{wind_speed}mph", 26, 2, wind_speed_color, BLACK)
         # Displaying wind direction as an arrow or text can be added here
 
     elif button == "d":
         # Display Cloud coverage % and visibility
-        cloud_coverage = weather_data['clouds']['all']
+        cloud_coverage = weather_data["clouds"]["all"]
         cloud_coverage_color = get_cloud_coverage_color(cloud_coverage)
-        visibility = weather_data.get('visibility', 'N/A')  # Some data might not always be present
-        visibility_color = get_visibility_color(visibility)
-        #display_jpeg("cloud_icon.jpg", 10, 10)
-        outline_text(f"{cloud_coverage}%", 2, 2, cloud_coverage_color, BLACK)
-        outline_text(f"{visibility}m", 20, 2, visibility_color, BLACK)
 
-    # Additional screens for rain or snow can be added similarly
-    rain = weather_data.get('rain', {})
-    snow = weather_data.get('snow', {})
-    
-    if rain:
-        # Display 1h and 3h rain amount
-        rain_1h = rain.get('1h', 'N/A')
-        rain_3h = rain.get('3h', 'N/A')
-        #display_jpeg("rain_icon.jpg", 10, 10)
-        outline_text(f"1h: {rain_1h} mm", 0, 2)
-        outline_text(f"3h: {rain_3h} mm", 25, 2)
-    elif snow:
-        # Display 1h and 3h snow amount
-        snow_1h = snow.get('1h', 'N/A')
-        snow_3h = snow.get('3h', 'N/A')
-        #display_jpeg("snow_icon.jpg", 10, 10)
-        outline_text(f"1h: {snow_1h} mm", 0, 2)
-        outline_text(f"3h: {snow_3h} mm", 25, 2)
-        
+        # Convert visibility from meters to kilometers
+        visibility_m = weather_data.get("visibility", "N/A")
+        if visibility_m != "N/A":
+            visibility_km = visibility_m / 1000
+            visibility_str = f"{visibility_km:.1f}km"
+        else:
+            visibility_str = "N/A"
+
+        visibility_color = get_visibility_color(visibility_m)
+        # display_jpeg("cloud_icon.jpg", 10, 10)
+        outline_text(f"{cloud_coverage}%", 2, 2, cloud_coverage_color, BLACK)
+        outline_text(visibility_str, 24, 2, visibility_color, BLACK)
+
+        # Additional screens for rain or snow can be added similarly
+        rain = weather_data.get("rain", {})
+        snow = weather_data.get("snow", {})
+
+        if rain:
+            # Display 1h and 3h rain amount
+            rain_1h = rain.get("1h", "N/A")
+            rain_3h = rain.get("3h", "N/A")
+            # display_jpeg("rain_icon.jpg", 10, 10)
+            outline_text(f"1h: {rain_1h} mm", 0, 2)
+            outline_text(f"3h: {rain_3h} mm", 25, 2)
+        elif snow:
+            # Display 1h and 3h snow amount
+            snow_1h = snow.get("1h", "N/A")
+            snow_3h = snow.get("3h", "N/A")
+            # display_jpeg("snow_icon.jpg", 10, 10)
+            outline_text(f"1h: {snow_1h} mm", 0, 2)
+            outline_text(f"3h: {snow_3h} mm", 25, 2)
+
+
 def display_jpeg(filename, x, y):
     j = jpegdec.JPEG(graphics)
     j.open_file(filename)
     j.decode(x, y, jpegdec.JPEG_SCALE_FULL)
 
+
 def adjust_brightness(delta):
     gu.adjust_brightness(delta)
 
+
 def clear_screen():
     graphics.clear()
+
 
 def outline_text(text, x, y, text_color=WHITE, bg_color=BLACK):
     graphics.set_pen(bg_color)
@@ -277,12 +320,13 @@ def outline_text(text, x, y, text_color=WHITE, bg_color=BLACK):
     graphics.text(text, x, y, -1, 1)
     graphics.set_pen(WHITE)
 
+
 # Main loop
 button_actions = {
     GalacticUnicorn.SWITCH_A: "a",
     GalacticUnicorn.SWITCH_B: "b",
     GalacticUnicorn.SWITCH_C: "c",
-    GalacticUnicorn.SWITCH_D: "d"
+    GalacticUnicorn.SWITCH_D: "d",
 }
 
 gu.set_brightness(0.5)
@@ -312,7 +356,10 @@ while True:
     elif current_display == "date":
         display_date()
 
-    if last_button_press_time and (time.time() - last_button_press_time) > BUTTON_PRESS_TIMEOUT:
+    if (
+        last_button_press_time
+        and (time.time() - last_button_press_time) > BUTTON_PRESS_TIMEOUT
+    ):
         current_display = "clock"
 
     brightness = gu.get_brightness()
