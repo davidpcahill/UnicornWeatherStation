8b4ffc44190dda0dac8a649d357c74a987d28764
Tag Wolf <david.cahill@gmail.com>
2023-09-10 05:32:29 -0700

    Update weatherstation.py

--- weatherstation.py
+++ weatherstation.py
@@ -1,14 +1,15 @@
 from galactic import GalacticUnicorn
 from picographics import PicoGraphics, DISPLAY_GALACTIC_UNICORN
+import math
 import network
 import ntptime
 import time
 import urequests as requests
 import jpegdec
 
-WIFI_SSID = "YOUR SSID"  # Replace with your SSID
-WIFI_PASSWORD = "YOUR WIFI PASSWORD"  # Replace with your password
-OPEN_WEATHER_MAP_API_KEY = "YOUR OPENWEATHER API KEY"  # Replace with your API key
+WIFI_SSID = "WIFI_SSID" # Replace with your Wi-Fi SSID
+WIFI_PASSWORD = "WIFI_PASSWORD" # Replace with your Wi-Fi password
+OPEN_WEATHER_MAP_API_KEY = "API_KEY" # Replace with your openweathermap.org API key
 CITY = "San Jose"  # Replace with your city
 COUNTRY_CODE = "US"  # Replace with your country code
 URL = f"http://api.openweathermap.org/data/2.5/weather?q={CITY},{COUNTRY_CODE}&units=imperial&APPID={OPEN_WEATHER_MAP_API_KEY}"
@@ -17,6 +18,14 @@ BUTTON_PRESS_TIMEOUT = (
     10  # Time in seconds after which the clock will be displayed again
 )
 WEATHER_FETCH_INTERVAL = 60
+# constants for controlling the background colour throughout the day
+MIDDAY_HUE = 1.1
+MIDNIGHT_HUE = 0.8
+HUE_OFFSET = -0.1
+MIDDAY_SATURATION = 1.0
+MIDNIGHT_SATURATION = 1.0
+MIDDAY_VALUE = 0.8
+MIDNIGHT_VALUE = 0.3
 
 gu = GalacticUnicorn()
 graphics = PicoGraphics(display=DISPLAY_GALACTIC_UNICORN)
@@ -31,6 +40,30 @@ BLUE = graphics.create_pen(24, 153, 241)
 display_clock = True
 last_button_press_time = None
 last_weather_fetch_time = 0
+
+
+@micropython.native  # noqa: F821
+def from_hsv(h, s, v):
+    i = math.floor(h * 6.0)
+    f = h * 6.0 - i
+    v *= 255.0
+    p = v * (1.0 - s)
+    q = v * (1.0 - f * s)
+    t = v * (1.0 - (1.0 - f) * s)
+
+    i = int(i) % 6
+    if i == 0:
+        return int(v), int(t), int(p)
+    if i == 1:
+        return int(q), int(v), int(p)
+    if i == 2:
+        return int(p), int(v), int(t)
+    if i == 3:
+        return int(p), int(q), int(v)
+    if i == 4:
+        return int(t), int(p), int(v)
+    if i == 5:
+        return int(v), int(p), int(q)
 
 
 def connect_to_wifi():
@@ -183,6 +216,17 @@ def get_date_color(month):
 
 def display_time():
     _, _, _, hour, minute, second, _, _ = time.localtime()
+
+    time_through_day = (((hour * 60) + minute) * 60) + second
+    percent_through_day = time_through_day / 86400
+    percent_to_midday = 1.0 - ((math.cos(percent_through_day * math.pi * 2) + 1) / 2)
+    hue = ((MIDDAY_HUE - MIDNIGHT_HUE) * percent_to_midday) + MIDNIGHT_HUE
+    sat = (
+        (MIDDAY_SATURATION - MIDNIGHT_SATURATION) * percent_to_midday
+    ) + MIDNIGHT_SATURATION
+    val = ((MIDDAY_VALUE - MIDNIGHT_VALUE) * percent_to_midday) + MIDNIGHT_VALUE
+    gradient_background(hue, sat, val, hue + HUE_OFFSET, sat, val)
+
     clock = "{:02}:{:02}:{:02}".format(hour, minute, second)
 
     # calculate text position so that it is centred
@@ -319,6 +363,28 @@ def outline_text(text, x, y, text_color=WHITE, bg_color=BLACK):
     graphics.set_pen(text_color)
     graphics.text(text, x, y, -1, 1)
     graphics.set_pen(WHITE)
+
+
+def gradient_background(start_hue, start_sat, start_val, end_hue, end_sat, end_val):
+    half_width = width // 2
+    for x in range(0, half_width):
+        hue = ((end_hue - start_hue) * (x / half_width)) + start_hue
+        sat = ((end_sat - start_sat) * (x / half_width)) + start_sat
+        val = ((end_val - start_val) * (x / half_width)) + start_val
+        colour = from_hsv(hue, sat, val)
+        graphics.set_pen(
+            graphics.create_pen(int(colour[0]), int(colour[1]), int(colour[2]))
+        )
+        for y in range(0, height):
+            graphics.pixel(x, y)
+            graphics.pixel(width - x - 1, y)
+
+    colour = from_hsv(end_hue, end_sat, end_val)
+    graphics.set_pen(
+        graphics.create_pen(int(colour[0]), int(colour[1]), int(colour[2]))
+    )
+    for y in range(0, height):
+        graphics.pixel(half_width, y)
 
 
 # Main loop
